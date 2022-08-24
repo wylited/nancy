c6c8cf7190f8a148f6d56e0c4de47ff5b64a2912
wylited <wylited@gmail.com>
2022-08-24 09:46:06 +0800

    gin initialization + reformat

--- main.go
+++ main.go
@@ -6,19 +6,38 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	// Connect to the mongodb server
-	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://localhost:27017"))
+
+	//Initialize gin connection
+	ginit()
+
+	// Initialize mongo connection
+	client := mongoInit()
+}
+
+func ginit() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run(":8080")
+}
+
+func mongoInit() *mongo.Client {
+	ctx := context.TODO()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
 	if err != nil {
 		panic(err)
 	}
-
-	// Check the connection
-	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
 		panic(err)
 	}
-
-	// usersCollection := client.Database("testing").Collection("users")
+	return client
 }
