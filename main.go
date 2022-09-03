1ce577ba986b0d51b0e3083a0db8c3847b57f177
wylited <wylited@gmail.com>
2022-09-03 22:59:08 +0800

    example mongo json's

--- main.go
+++ main.go
@@ -5,39 +5,52 @@ import (
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/mongo/readpref"
 
 	"github.com/gin-gonic/gin"
 )
 
+type Account string
+
+const (
+	checking Account = "checking"
+	savings  Account = "savings"
+	credit   Account = "credit card"
+	invest   Account = "investment"
+	mortage  Account = "mortage"
+	other    Account = "other"
+)
+
 func main() {
+	uri := "mongodb://localhost:27017" //!TODO Make this config editable.
 
 	//Initialize gin connection
-	ginit()
+	r := gin.Default()
+
+	r.Run(":8080") //!TODO Make the port config client adjustable.
 
 	// Initialize mongo connection
-	client := mongoInit()
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+
+	if err != nil {
+		panic(err) //!TODO throw more sustainable errors
+	}
+
+	na := client.Database("HSBC") // current example
 }
 
-func ginit() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run(":8080")
-}
+// func ginInit() {
+// 	r := gin.Default()
 
-func mongoInit() *mongo.Client {
-	ctx := context.TODO()
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
-	if err != nil {
-		panic(err)
-	}
-	err = client.Ping(ctx, readpref.Primary())
-	if err != nil {
-		panic(err)
-	}
-	return client
-}
+// 	r.Run(":8080") //!TODO Make the port config client adjustable.
+// }
+
+// func mongoInit(uri string) *mongo.Database {
+// 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+
+// 	if err != nil {
+// 		panic(err) //!TODO throw more sustainable errors
+// 	}
+
+// 	na := client.Database("nancy-account")
+// 	return na
+// }
