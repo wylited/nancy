fd0e28223005cf2d72b55f0af1a4c03a639a31d5
wylited <wylited@gmail.com>
2022-10-24 21:03:44 +0800

    Revert "gitignore"
    This reverts commit b92941d861d3e37c7855564a4648f1ac9abe9a7b.

--- server/target/debug/deps/nancy_server-1c1e89d93fdb4f9c.d
+++
@@ -1,5 +0,0 @@
-/home/wyli/Repositories/nancy/server/target/debug/deps/nancy_server-1c1e89d93fdb4f9c.rmeta: src/main.rs
-
-/home/wyli/Repositories/nancy/server/target/debug/deps/nancy_server-1c1e89d93fdb4f9c.d: src/main.rs
-
-src/main.rs:
--- server/target/debug/deps/nancy_server-c62b36588e71c483.d
+++
@@ -1,5 +0,0 @@
-/home/wyli/Repositories/nancy/server/target/debug/deps/nancy_server-c62b36588e71c483.rmeta: src/main.rs
-
-/home/wyli/Repositories/nancy/server/target/debug/deps/nancy_server-c62b36588e71c483.d: src/main.rs
-
-src/main.rs:
