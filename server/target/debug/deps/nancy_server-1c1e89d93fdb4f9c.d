b92941d861d3e37c7855564a4648f1ac9abe9a7b
wylited <wylited@gmail.com>
2022-10-24 21:00:54 +0800

    gitignore

---
+++ server/target/debug/deps/nancy_server-1c1e89d93fdb4f9c.d
@@ -0,0 +1,5 @@
+/home/wyli/Repositories/nancy/server/target/debug/deps/nancy_server-1c1e89d93fdb4f9c.rmeta: src/main.rs
+
+/home/wyli/Repositories/nancy/server/target/debug/deps/nancy_server-1c1e89d93fdb4f9c.d: src/main.rs
+
+src/main.rs:
