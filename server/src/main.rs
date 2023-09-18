355523a6dd0b6cd1a5ccb97c57f1793c540bac94
wylited <44154103+wylited@users.noreply.github.com>
2023-09-18 16:06:35 +0800

    init

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,33 +1,12 @@
-pub mod accounts;
-pub mod transactions;
-
-use accounts::{get_accounts, add_account, get_accounttypes};
-use anyhow::Result;
 use axum::{routing::get, Router};
-use transactions::{get_categories, get_transactions};
-use std::net::SocketAddr;
 
-#[derive(Clone)]
-pub struct Db {
-    pub client: edgedb_tokio::Client
+async fn hello_world() -> &'static str {
+    "Hello, world!"
 }
 
-#[tokio::main]
+#[shuttle_runtime::main]
+async fn axum() -> shuttle_axum::ShuttleAxum {
+    let router = Router::new().route("/", get(hello_world));
 
-async fn main() -> Result<()> {
-    let db = Db { client: edgedb_tokio::create_client().await? };
-
-    let router = Router::new()
-        .route("/api/accounts", get(get_accounts).post(add_account))
-        .route("/api/accounttypes", get(get_accounttypes))
-        .route("/api/categories", get(get_categories))
-        .route("/api/transactions", get(get_transactions))
-        .with_state(db);
-
-
-    axum::Server::bind(&SocketAddr::from(([127, 0, 0, 1], 3000)))
-        .serve(router.into_make_service())
-        .await?;
-
-    Ok(())
+    Ok(router.into())
 }
